89f9e42eeae36f368d31ed12926969d0308131f0
suraj5 <kmit.suraj@gmail.com>
2014-08-07 17:27:45 +0530

    GroupChat-SampleAppplication

---
+++ GroupChat/app.js
@@ -0,0 +1,31 @@
+var express = require('express')
+  , app = express.createServer()
+  , io  = require('socket.io').listen(app);
+
+app.use(app.router);
+app.use(express.static(__dirname + '/public'));
+app.listen(process.env.VMC_APP_PORT || 1337);
+
+app.get('/', function(req, res){
+  res.sendfile(__dirname + '/index.html');
+});
+
+
+io.sockets.on('connection', function (socket) {
+  
+  socket.on('set nickname', function(nick){
+    socket.set('nickname', nick, function(){
+      socket.emit('ready');
+      socket.emit('clnt', {nick: {name:'SYSTEM',color:'red'}, msg: 'Welcome ' + nick.name});
+    });
+  });
+  
+  socket.on('msg', function (data) {
+    if( data.length > 0 ){
+      socket.get('nickname', function(err, name){
+        io.sockets.emit('clnt', {nick: name, msg: data});
+      });
+    }
+  });
+  
+});
